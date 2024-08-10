fd16f7a96d29b8722e34f7ad9515ee25e4c212fd
Kiyarash Software Developer <hasanzadekiarash7@gmail.com>
2024-08-10 17:36:45 +0330

    add card templates and header &footer components for starting who we are page

--- src/pages/WhoWeAre.tsx
+++ src/pages/WhoWeAre.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import NavBar from '../components/Navbar'
+import CardTemplate from '../components/CardTemplate'
+import LmFooter from '../components/LmFooter'
 
 const WhoWeAre:React.FC = ():React.ReactNode => {
+  const whoCards = [
+    {
+      title: 'What the Mayor does',
+      img: 'https://www.london.gov.uk/sites/default/files/styles/media_component_16_9_lg/public/2024-02/Large_Web_Image-Mayor-of-London-Sadiq-Khan.jpg?h=2a71c125&itok=6x6roV00',
+      description: 'Sadiq Khan is the current Mayor. He provides citywide leadership and creates policies to improve London for all.'
+    },
+    {
+      title: 'What the London Assembly does',
+      img: 'https://www.london.gov.uk/sites/default/files/styles/media_component_16_9_lg/public/chamber_cityhall_992x496_31mar22_2.jpg?h=6258ffef&itok=3t4mmyNp',
+      description: 'The Assembly holds the Mayor and Mayoral advisers to account by examining their work to ensure promises to Londoners are delivered.'
+    },
+  ]
   return (
     <div>
       <NavBar />
-      <h1 className='text-black'>hello who we are page</h1>
+      <CardTemplate title='Who we are' cards={whoCards} />
+      <CardTemplate title='Who we are' cards={whoCards} />
+      <CardTemplate title='Who we are' cards={whoCards} />
+      <CardTemplate title='Who we are' cards={whoCards} />
+      <LmFooter />
     </div>
   )
 }
